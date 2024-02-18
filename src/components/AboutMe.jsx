d50b2111f7e3f6b0e5c64756fe98d1073c03b958
asobkovskaia <a.sobkovskaia@gmail.com>
2024-02-18 19:50:21 +0100

    AboutMe & Skills section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,10 +1,32 @@
 import '../styles.css'
-import React from 'react';
+import React from 'react'
+import myPhoto from '../assets/logo192.png'
 
 export default function AboutMe() {
-    return(
-        <section className='about-me'>
-            
-        </section>
-    );
+  return (
+    <section className="about-me">
+      <div className="greeting">
+        <h1>Anastasiia here! 🧡</h1>
+      </div>
+      <div className="about-me-info">
+        <div className="about-me-text">
+          <div className="text-wrapper">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quam laudantium, dolor voluptate voluptas, vero cumque ad,
+              autem sunt voluptatem sapiente maxime suscipit doloremque delectus corrupti sint quas deleniti facilis!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, deserunt vel aliquid esse similique accusantium deleniti
+              impedit ab exercitationem omnis! Nemo adipisci est in itaque. Nesciunt ullam minima neque. Ullam.
+            </p>
+          </div>
+        </div>
+        <div className="about-me-foto">
+          <div className="foto-wrapper">
+            <img src={myPhoto} alt="myPhoto" width="200" height="200" />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,6 +4,49 @@ import React from 'react';
 export default function Skills() {
     return(
         <section className='skills'>
+            <div className='skills-header'>
+                <h2>These are my skills</h2>
+                <p>So much skills, so many smArT</p>
+            </div>
+            <div className='skills-list'>
+            <div className='frontend'>
+                <h4>Frontend</h4>
+                <ul>
+                    <li>TypeScript</li>
+                    <li>Web Components</li>
+                    <li>React</li>
+                    <li>JavaScript</li>
+                    <li>Flutter</li>
+                </ul>
+                </div>
+                <div className='devops'>
+                    <h4>DevOps</h4>
+                <ul>
+                    <li>Docker</li>
+                    <li>GitHub</li>
+                    <li>Jenkins</li>
+                </ul>
+                </div>
+                <div className='backend'>
+                    <h4>Backend</h4>
+                <ul>
+                    <li>Java</li>
+                    <li>Python</li>
+                    <li>MongoDB</li>
+                </ul>
+                </div>
+                <div className='languages'>
+                    <h4>Languages</h4>
+                <ul>
+                    <li>German</li>
+                    <li>English</li>
+                    <li>Russian</li>
+                    <li>Italian</li>
+                </ul>
+                </div>
+            
+            </div>
+                
             
         </section>
     );
