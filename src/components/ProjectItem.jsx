e4a2613266ad1254b8335b2fc180c94c29d2213d
asobkovskaia <a.sobkovskaia@gmail.com>
2024-05-25 20:46:07 +0200

    add box container component; restyle skill section

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import styles from './styles.module.css'
-import image from '../assets/logo192.png';
+import image from '../assets/logo_short.svg';
 
 export default function ProjectItem() {
   return (
     <div className={styles.projectItem}>
       <div className={styles.projectImage}>
-        <img src={image} alt="project-logo" />
+        <img src={image} alt="project-logo" height={30} width={30} />
       </div>
       <div className={styles.projectText}>
         <p>
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,53 +1,22 @@
 import '../styles.css'
-import React from 'react';
+import React from 'react'
+import tsIcon from '../assets/skills/typescript.svg'
+import BoxContainer from '../components/BoxContainer.jsx'
 
 export default function Skills() {
-    return(
-        <section id="skills" className='skills wrapper'>
-            <div className='skills-header'>
-                <h2>These are my skills</h2>
-                <p>So much skills, so many smArT</p>
-            </div>
-            <div className='skills-list'>
-            <div className='frontend'>
-                <h4>Frontend</h4>
-                <ul>
-                    <li>TypeScript</li>
-                    <li>Web Components</li>
-                    <li>React</li>
-                    <li>JavaScript</li>
-                    <li>Flutter</li>
-                </ul>
-                </div>
-                <div className='devops'>
-                    <h4>DevOps</h4>
-                <ul>
-                    <li>Docker</li>
-                    <li>GitHub</li>
-                    <li>Jenkins</li>
-                </ul>
-                </div>
-                <div className='backend'>
-                    <h4>Backend</h4>
-                <ul>
-                    <li>Java</li>
-                    <li>Python</li>
-                    <li>MongoDB</li>
-                </ul>
-                </div>
-                <div className='languages'>
-                    <h4>Languages</h4>
-                <ul>
-                    <li>German</li>
-                    <li>English</li>
-                    <li>Russian</li>
-                    <li>Italian</li>
-                </ul>
-                </div>
-            
-            </div>
-                
-            
-        </section>
-    );
+    const frontendStack = ['TypeScript', 'React', 'Angular', 'CSS']
+  return (
+    <section id="skills" className="skills wrapper">
+      <div className="skills-header">
+        <h2>These are my skills</h2>
+        <p>So much skills, so many smArT</p>
+      </div>
+      <div className="skills-list">
+        <BoxContainer headline={"Frontend"} technologies={frontendStack}/>
+        <BoxContainer headline={"Backend"} technologies={frontendStack}/>
+        <BoxContainer headline={"DevOps"} technologies={frontendStack}/>
+        <BoxContainer headline={"Languages"} technologies={frontendStack}/>
+      </div>
+    </section>
+  )
 }
